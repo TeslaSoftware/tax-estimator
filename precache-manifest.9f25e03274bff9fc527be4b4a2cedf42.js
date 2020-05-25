45dc8a11e8d7c69d1a1fd52d5e41ead0deb9639c
Sebastian Tysler <stysler@gmail.com>
2020-05-24 21:11:36 -0500

    Updates

---
+++ precache-manifest.9f25e03274bff9fc527be4b4a2cedf42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75206af21b87cfdcffa07a063db2bc24",
+    "url": "/tax-estimator/index.html"
+  },
+  {
+    "revision": "89f0b45d905d7d3e8021",
+    "url": "/tax-estimator/static/css/main.99535029.chunk.css"
+  },
+  {
+    "revision": "661e38f8bb91c7913097",
+    "url": "/tax-estimator/static/js/2.8d42ef4e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tax-estimator/static/js/2.8d42ef4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f0b45d905d7d3e8021",
+    "url": "/tax-estimator/static/js/main.a66b1ddf.chunk.js"
+  },
+  {
+    "revision": "c1ea3832a7e042b804cc",
+    "url": "/tax-estimator/static/js/runtime-main.1d1fdf95.js"
+  }
+]);
